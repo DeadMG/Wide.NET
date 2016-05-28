f096dcb6453f68656e87e8af31123efbad407eea
DeadMG <wolfeinstein@gmail.com>
2016-05-28 22:29:59 +0100

    Add integer test

--- Wide/LexerTest/VariableLengthTokenTests.cs
+++ Wide/LexerTest/VariableLengthTokenTests.cs
@@ -54,5 +54,19 @@ namespace Wide.LexerTest
             Assert.AreEqual(results[0].Location.Begin, startPosition);
             Assert.AreEqual(results[0].Location.End, sampleInput.Aggregate(startPosition, (pos, character) => pos.Advance(character, lexer.tabsize)));
         }
+
+        [Test]
+        public void TestIntegers()
+        {
+            var lexer = new Lexer();
+            var sampleInput = "11111";
+            var startPosition = new SourcePosition(1, 0, 0);
+            var results = lexer.Lex(sampleInput, startPosition).ToList();
+            Assert.That(results.Count == 1);
+            Assert.That(results[0].Type == PredefinedTokenTypes.Integer);
+            Assert.That(results[0].Value == sampleInput);
+            Assert.AreEqual(results[0].Location.Begin, startPosition);
+            Assert.AreEqual(results[0].Location.End, sampleInput.Aggregate(startPosition, (pos, character) => pos.Advance(character, lexer.tabsize)));
+        }
     }
 }
